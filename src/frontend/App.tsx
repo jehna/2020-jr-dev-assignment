fa86fa54fd92d62d458995a1ecc785efaf6dd766
julius turunen <julius.turunen@aalto.fi>
2020-11-30 04:55:33 +0200

    Added ItemsTable component and Pagination

--- src/frontend/App.tsx
+++ src/frontend/App.tsx
@@ -1,22 +1,109 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { ping, selectMessage } from './pingSlice';
+import { Alert, Container, Dropdown, DropdownButton, Pagination } from 'react-bootstrap';
+import { connect } from 'react-redux';
+import { Item } from '../shared/types';
+import ItemsTable from './ItemsTable';
+import { RootState } from './store';
 
-function App() {
-  const state = useSelector(selectMessage);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <h1>
-        {state}
-      </h1>
-      <button
-        onClick={() => dispatch(ping())}
-      >
-        Click
-      </button>
-    </div>
-  );
+interface AppProps {
+  items: Item[];
+  isLoading: boolean;
 }
 
-export default App;
+interface AppState {
+  perPage: number;
+  pageNum: number;
+}
+
+function mapStateToProps(state: RootState) {
+  const { items, isLoading } = state.data;
+  return { items, isLoading } as AppProps;
+}
+
+class App extends React.Component<AppProps, AppState> {
+  constructor(props: AppProps) {
+    super(props);
+    this.state = {
+      perPage: 25,
+      pageNum: 1
+    }
+  }
+
+  itemsToRender() {
+    // If no items loaded
+    if(!this.props.items) return [];
+    const items = [];
+    const len = this.props.items.length;
+    const [n, per] = [this.state.pageNum, this.state.perPage];
+    const start = (n - 1) * per;
+    const end = Math.min(start + per, len);
+    for (let i = start; i < end; ++i) {
+      items.push(this.props.items[i]);
+    }
+    return items;
+  }
+
+  maxPages() {
+    const len = this.props.items.length;
+    const per = this.state.perPage;
+    return Math.ceil(len/per);
+  }
+
+  setPage(n: number) {
+    this.setState({pageNum: n});
+  }
+
+  setPer(n: number) {
+    this.setState({perPage: n});
+  }
+
+  render() {
+    const items = this.itemsToRender();
+    const options = [10, 25, 50];
+    const pageN = this.state.pageNum;
+    const maxN = this.maxPages();
+    const c1 = pageN === 1;
+    const c2 = pageN === maxN;
+    return (
+      <Container>
+        <h1>Reaktor Warehouse</h1>
+        <Alert show={this.props.isLoading}>Loading...</Alert>
+        <ItemsTable items={items} />
+        <Pagination>
+          <DropdownButton title={`${this.state.perPage} per page`}>
+            {
+              options.map(n => 
+                <Dropdown.Item
+                  onClick={() => this.setPer(n)}
+                >
+                  {n}
+                </Dropdown.Item>
+              )
+            }
+          </DropdownButton>
+          <Pagination.First
+            disabled={c1}
+            onClick={() => this.setPage(1)}
+          />
+          <Pagination.Prev 
+            disabled={c1}
+            onClick={() => this.setPage(pageN - 1)}
+          />
+          <Pagination.Item disabled>
+            {`${pageN} / ${maxN}`}
+          </Pagination.Item>
+          <Pagination.Next 
+            disabled={c2}
+            onClick={() => this.setPage(pageN + 1)}
+          />
+          <Pagination.Last
+            disabled={c2}
+            onClick={() => this.setPage(maxN)}
+          />
+        </Pagination>
+      </Container>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(App);
---
+++ src/frontend/ItemsTable.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Badge, Table } from 'react-bootstrap';
+import { Item } from '../shared/types';
+
+interface ItemsTableProps {
+  items: Item[];
+}
+
+export default function ItemsTable(props: ItemsTableProps) {
+  return (
+    <Table striped bordered hover responsive='md'>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Category</th>
+          <th>Manufacturer</th>
+          <th>Price</th>
+          <th>Availability</th>
+        </tr>
+      </thead>
+      <tbody>
+        { props.items.map(renderItem) }
+      </tbody>
+    </Table>
+  );
+}
+
+function renderItem(item: Item) {
+  const {id, type, name, /*color,*/ price, manufacturer, stock} = item;
+  return (
+    <tr key={id}>
+      <th>{name}</th>
+      <th>{type}</th>
+      <th>{manufacturer}</th>
+      <th>{price}</th>
+      <th>{renderStock(stock)}</th>
+    </tr>
+  );
+}
+
+function renderStock(n: number | undefined) {
+  let [text, variant] = ['N/A', 'info'];
+  if (n) {
+    switch(n) {
+      case 2: 
+        text = 'In stock';
+        variant = 'success';
+        break;
+      case 1:
+        text = 'Less than 10';
+        variant = 'warning';
+        break;
+      case 0:
+        text = 'Out of stock';
+        variant = 'danger';
+        break;
+    }
+  }
+  return (
+    <Badge variant={variant} >
+      {text}
+    </Badge>
+  );
+}
