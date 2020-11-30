64dce33d33a771e4d324bd54e3bf8a1dad870074
julius turunen <julius.turunen@aalto.fi>
2020-11-30 04:54:44 +0200

    Loads data from server on page load

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import App from './frontend/App';
 import { store } from './frontend/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './frontend/serviceWorker';
+import { updateItems } from './frontend/data';
+
+// Load initial data
+store.dispatch(updateItems());
 
 ReactDOM.render(
   <React.StrictMode>
